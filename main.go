245ad9b110b00ed82b252fb54e70688294f4abf5
sinasheikholeslami <sina@sinasheikholeslami.com>
2024-08-06 18:08:03 -0700

    declare vars

--- main.go
+++ main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Sina"
+	fmt.Println(whatToSay)
+
+	i = 7
+	fmt.Println("i is set to", i)
+
+	whatWasSaid := saySomething()
+	fmt.Println(whatWasSaid)
+}
+
+func saySomething() string {
+	return "something"
 }
