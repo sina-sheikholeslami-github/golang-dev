710417cae76d403228621b319947d55d13981c42
sinasheikholeslami <sina@sinasheikholeslami.com>
2024-08-07 10:29:52 -0700

    add struct

--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName string
+	LastName string
+	Age int
+	BirthDate time.Time
+}
 
 func main() {
-	fmt.Println("Hello World!")
-
-	var whatToSay string
-	var i int
-
-	whatToSay = "Sina"
-	fmt.Println(whatToSay)
-
-	i = 7
-	fmt.Println("i is set to", i)
-
-	whatWasSaid := saySomething()
-	fmt.Println(whatWasSaid)
-
-	changeUsingPointer(&whatWasSaid)
-
-	fmt.Println(whatWasSaid)
-}
-
-func saySomething() string {
-	return "something"
-}
-
-func changeUsingPointer(s *string) {
-	newValue := "Red"
-	*s = newValue
-
+	user := User {
+		FirstName: "Sina",
+		LastName: "Sheikholeslami",
+	}
+	log.Println(user)
 }
